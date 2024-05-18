f0b4f875bc9a1bfddf365db7fe5ac71bc07fcdc4
Nirjaraa <nirjarathapa11@gmail.com>
2024-05-18 12:26:13 +0545

    signup connected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import "./index.css";
-import {Route,Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Profile from "./routes/Profile";
 import Log from "./routes/Log";
+import axios from "axios";
+import { useEffect } from "react";
 
-function App(){
-  return(
+function App() {
+  useEffect(() => {}, []);
+  return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/log" element={<Log/>}/>
-
-
-      </Routes>  
-       
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/log" element={<Log />} />
+      </Routes>
     </div>
-
-    
   );
 }
-export default App; 
+export default App;
--- src/routes/Log.js
+++ src/routes/Log.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import './LogStyles.css';
+import React, { useState } from "react";
+import "./LogStyles.css";
 const AccountForm = () => {
-  const [formType, setFormType] = useState('signup'); // 'signup' or 'login'
+  const [formType, setFormType] = useState("signup"); // 'signup' or 'login'
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    phoneNumber: '',
-    address: '',
-    dateOfBirth: '',
-    gender: '',
-    role: 'client', // default to 'client'
-    profession: ''
+    fullName: "",
+    email: "",
+    password: "",
+    phoneNumber: "",
+    address: "",
+    DOB: "",
+    gender: "",
+    role: "client", // default to 'client'
+    //profession: "",
   });
 
   const handleInputChange = (event) => {
@@ -19,28 +19,45 @@ const AccountForm = () => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Submit form data to your server or API endpoint
-    console.log(formData);
+    try {
+      const body = {
+        ...formData,
+        DOB: new Date(formData.DOB),
+      };
+      const response = await fetch("http://localhost:5000/user/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+      }
+      console.log("User registered successfully!");
+    } catch (error) {
+      console.error("Error registering user:", error.message);
+    }
   };
 
   const toggleFormType = () => {
-    setFormType(formType === 'signup' ? 'login' : 'signup');
+    setFormType(formType === "signup" ? "login" : "signup");
   };
 
   return (
     <div>
-      
-      {formType === 'signup' && (
+      {formType === "signup" && (
         <form onSubmit={handleSubmit}>
           <h1>Signup</h1>
-          <input type="text" name="name" placeholder="Name" onChange={handleInputChange} required />
+          <input type="text" name="fullName" placeholder="Name" onChange={handleInputChange} required />
           <input type="email" name="email" placeholder="Email" onChange={handleInputChange} required />
           <input type="password" name="password" placeholder="Password" onChange={handleInputChange} required />
           <input type="tel" name="phoneNumber" placeholder="Phone Number" onChange={handleInputChange} required />
           <input type="text" name="address" placeholder="Address" onChange={handleInputChange} required />
-          <input type="date" name="dateOfBirth" placeholder="Date of Birth" onChange={handleInputChange} required />
+          <input type="date" name="DOB" placeholder="Date of Birth" onChange={handleInputChange} required />
           <select name="gender" onChange={handleInputChange} required>
             <option value="">Select Gender</option>
             <option value="male">Male</option>
@@ -51,7 +68,7 @@ const AccountForm = () => {
             <option value="client">Client</option>
             <option value="professional">Professional</option>
           </select>
-          {formData.role === 'professional' && (
+          {formData.role === "professional" && (
             <select name="profession" onChange={handleInputChange} required>
               <option value="">Select Profession</option>
               <option value="medical">Medical</option>
@@ -61,21 +78,17 @@ const AccountForm = () => {
             </select>
           )}
           <button type="submit">Sign Up</button>
-          <button onClick={toggleFormType}>
-         {formType === 'signup' ? 'Login' : 'Signup'}
-      </button>
+          <button onClick={toggleFormType}>{formType === "signup" ? "Login" : "Signup"}</button>
         </form>
       )}
-      {formType === 'login' && (
+      {formType === "login" && (
         <form onSubmit={handleSubmit}>
           <h1>Login</h1>
           <input type="text" name="name" placeholder="Name" onChange={handleInputChange} required />
           <input type="email" name="email" placeholder="Email" onChange={handleInputChange} required />
           <input type="password" name="password" placeholder="Password" onChange={handleInputChange} required />
           <button type="submit">Login</button>
-          <button onClick={toggleFormType}>
-          {formType === 'signup' ? 'Login' : 'Signup'}
-      </button>
+          <button onClick={toggleFormType}>{formType === "signup" ? "Login" : "Signup"}</button>
         </form>
       )}
     </div>
